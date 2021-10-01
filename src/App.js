53808f8b0b42869cd99209addfa98ee3b763b1ae
Andrew Salber <salber.andrew@gmail.com>
2021-09-30 21:15:57 -0700

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { react } from '@babel/types';
+import { render } from '@testing-library/react';
+import React from 'react';
+import QuoteMachine from './components/QuoteMachine';
+import 'typeface-roboto';
+import {Grid, withStyles} from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+  container: {
+    display: 'flex',
+    height: '100vh',
+    alignItems: 'center'
+  }
+}
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      selectedQuoteIndex: null
+    };
+
+    this.selectedQuoteIndex = this.selectedQuoteIndex.bind(this);
+  };
+
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+    .then(data => data.json())
+    .then(quotes => this.setState({quotes: quotes}, this.selectedQuoteIndex))
+  }
+
+  selectedQuoteIndex () {
+    const random = Math.random();
+    const index = this.state.quotes.length * random;
+    const roundedIndex = Math.floor(index)
+    this.setState({
+      selectedQuoteIndex: roundedIndex
+    });
+  }
+
+  get selectedQuote() {
+    if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
+      return;
+    }
+    return this.state.quotes[this.state.selectedQuoteIndex];
+  }
+
+  render() {
+    return (
+      <Grid className = {this.props.classes.container} id="quote-box" justifyContent="center" container>
+        <Grid xs={8} lg={6} item>
+          <QuoteMachine selectedQuote = {this.selectedQuote} selectedQuoteIndex = {this.selectedQuoteIndex}/>
+        </Grid>
+      </Grid>
+    );
+  };
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/components/QuoteMachine.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import IconButton from '@material-ui/core/IconButton'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+const QuoteMachine = (props) => (
+    <Card>
+        <CardContent id="text">
+            <Typography>
+                {props.selectedQuote ? '"' + props.selectedQuote.quote + '"' : ""}
+            </Typography>
+            <div id="author">
+                {props.selectedQuote ? ' -' + props.selectedQuote.author : ""} 
+            </div>
+            <br />
+        </CardContent>
+
+        <CardActions>
+            <Button size='small' onClick = {props.selectedQuoteIndex}>Next Quote</Button>
+            <IconButton id="tweet-quote" href={encodeURI("twitter.com/intent/tweet")} target="_blank">
+                <FontAwesomeIcon icon={faTwitter} size= "lg"></FontAwesomeIcon>
+            </IconButton>
+        </CardActions>
+        
+    </Card>
+)
+
+export default QuoteMachine;
